b5827cc2062ce9fd00e1e4a90c36759044c27600
danielturato <dturato@hotmail.com>
2021-11-15 15:10:41 +0000

    Updated worker redis dsn

--- app/config.py
+++ app/config.py
@@ -4,7 +4,6 @@ from pydantic import BaseSettings, RedisDsn
 class Settings(BaseSettings):
     profile: str = "DEV"
     mongodb_url: str = "mongodb://root:password@localhost:27017"
-    redistogo_url: RedisDsn = "redis://localhost:6379"
     base_cmg_url: str = "https://www.checkmategaming.com"
     cmg_game_path: str = "/tournament/cross-platform/call-of-duty-vanguard"
     base_gb_url: str = "https://gamebattles.majorleaguegaming.com/tournaments"
--- app/worker.py
+++ app/worker.py
@@ -1,11 +1,10 @@
+import os
 import redis
 from rq import Worker, Queue, Connection
-from .depenencies import get_settings
 
 listen = ['high', 'default', 'low']
-settings = get_settings()
-
-conn = redis.from_url(settings.redistogo_url)
+redis_url = os.getenv("REDISTOGO_URL", "redis://localhost:6379")
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
